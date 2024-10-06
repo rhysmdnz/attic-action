3fa8dd509c0958cd7f1aa29a48d8341ed9f3bac6
Rhys Davies <rhys@memes.nz>
2024-10-06 22:21:07 +1100

    profile the install

--- src/stages/install.ts
+++ src/stages/install.ts
@@ -9,24 +9,9 @@ export const install = async () => {
 	core.startGroup("Install attic");
 
 	core.info("Installing attic");
-	const installScript = await fetch(
-		"https://raw.githubusercontent.com/zhaofengli/attic/main/.github/install-attic-ci.sh",
-	).then((r) => {
-		if (!r.ok) {
-			core.setFailed(`Action failed with error: ${r.statusText}`);
-			core.endGroup();
-			process.exit(1);
-		}
-
-		return r.text();
-	});
 
 	try {
-		const installScriptPath = join(tmpdir(), "install-attic-ci.sh");
-
-		await writeFile(installScriptPath, installScript);
-		core.info("Running install script");
-		await exec("bash", [installScriptPath]);
+		await exec("nix", ["profile", "install", "github:zhaofengli/attic"]);
 	} catch (e) {
 		core.setFailed(`Action failed with error: ${e}`);
 	}
