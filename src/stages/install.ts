b8e9b79a1f1abf1edc8b95e19a3325c968e6e934
Rhys Davies <rhys@memes.nz>
2024-10-22 22:01:53 +1300

    from nixpkgs

--- src/stages/install.ts
+++ src/stages/install.ts
@@ -11,7 +11,7 @@ export const install = async () => {
 	core.info("Installing attic");
 
 	try {
-		await exec("nix", ["profile", "install", "github:zhaofengli/attic#attic-client"]);
+		await exec("nix", ["profile", "install", "nixpkgs#attic-client"]);
 	} catch (e) {
 		core.setFailed(`Action failed with error: ${e}`);
 	}
